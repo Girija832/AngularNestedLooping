fa6854a61d7be74b95c769a0357e70b015d9a44f
Girija <g.sahoo@3mdigital.co.in>
2018-07-28 20:41:23 +0530

    download pdf implimented

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { GetEmployeeListService } from '../services/get-employee-list.service';
 import { Location } from '@angular/common';
-
+import * as jsPDF from 'jspdf';
 
 @Component({
     selector: 'app-employee-details',
@@ -10,6 +10,7 @@ import { Location } from '@angular/common';
     styleUrls: ['./employee-details.component.css']
 })
 export class EmployeeDetailsComponent implements OnInit {
+    @ViewChild('content') content: ElementRef;
     userDetails: any = {};
     details: any;
     
@@ -27,6 +28,27 @@ export class EmployeeDetailsComponent implements OnInit {
         let rawData: any =  localStorage.getItem('currentUser');
         let parsedData: any = JSON.parse(rawData);
         this.details = parsedData;
+    }
+
+    public downloadPDF() {
+        let doc = new jsPDF({
+            unit: 'pt',
+        });
+
+        let specialElementHandlers = {
+            '#editor': function(element, renderer) {
+                return true;
+            }
+        };
+
+        let content = this.content.nativeElement;
+
+        doc.fromHTML(content.innerHTML, 15, 15, {
+            'width': 180,
+            'elementHandlers': specialElementHandlers
+        });
+
+        doc.save('sample.pdf');
     }
 
     goBack(): void {
